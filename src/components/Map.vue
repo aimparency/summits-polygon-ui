8c92072443b9f0fbdbd0e7e6dcdab0bfe43c4c75
Felix Niemeyer <niemeyer.felix@gmail.com>
2022-05-31 02:12:45 +0200

    wip Map: mouse panning + wheel zoom work

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -2,7 +2,7 @@
   <svg 
     ref="canvas"
     class='aim-map'
-    viewBox="-1000 -1000 2000 2000">
+    :viewBox="viewBox">
     <g :transform="transform">
       <FlowSVG v-for="flow in flows" 
         :key="`${flow.from}x${flow.into}`"
@@ -40,7 +40,7 @@ export default defineComponent({
   data() {
     return {
       aimNetwork: useAimNetwork(),
-      map: useMap()
+      map: useMap(), 
     }
   }, 
   props: {
@@ -48,46 +48,54 @@ export default defineComponent({
   },
   mounted() {
     const canvas = this.$refs.canvas as SVGElement
-    canvas.addEventListener("mousemove", (e: MouseEvent) => {
-      console.log("mouse moving!")
-      updateWhatever([e.clientX, e.clientY])
-    })
+
+    const updateHalfSide = () => {
+      let w = canvas.clientWidth 
+      let h = canvas.clientHeight
+      if(w < h) {
+        this.map.clientOffset = Vec2.fromValues(
+          0,
+          (h - w) / 2
+        )
+        this.map.halfSide = w / 2
+      } else {
+        this.map.clientOffset = Vec2.fromValues(
+          (w - h) / 2, 
+          0 
+        )
+        this.map.halfSide = h / 2
+      }
+    }
+    canvas.addEventListener("resize", updateHalfSide)
+    updateHalfSide()
 
     const updatePan = (mouse: V2) => {
       const pb = this.map.panBeginning; 
       if(pb !== undefined) {
-        const d = Vec2.clone(pb) 
+        const d = Vec2.clone(pb.page) 
         Vec2.sub(d, d, mouse) 
         if(Vec2.len(d) > 5) {
           this.map.preventReleaseClick = true
-          const halfSide = Math.min(
-            canvas.clientWidth,
-            canvas.clientHeight
-          ) / 2;
-          const scale = this.map.scale; 
-          Vec2.scale(d, d, 1 / (halfSide * scale)) 
+          Vec2.scale(d, d, this.map.logicalHalfSide / (this.map.halfSide * this.map.scale)) 
           const offset = Vec2.clone(pb.offset)
-          Vec2.add(offset, offset, d)
+          Vec2.sub(offset, offset, d)
           this.map.offset = offset  
         }
       }
     }
+
     const updateDrag = (mouse: V2) => {
-      console.log("update drag") 
       const db = this.map.dragBeginning;
       const aim = this.aimNetwork.dragCandidate; 
       if(db && aim) {
-        const dx = db.page.x - x
-        const dy = db.page.y - y
-        if(dx * dx + dy * dy > 5 * 5) {
+        const d = Vec2.clone(db.page)
+        Vec2.sub(d, d, mouse) 
+        if(Vec2.len(d) > 5) {
           this.map.preventReleaseClick = true
-          const halfSide = Math.min(
-            canvas.clientWidth, 
-            canvas.clientHeight
-          ) / 2;
-          const scale = this.map.scale; 
-          aim.x = db.initialPosition.x - dx / halfSide / scale; 
-          aim.y = db.initialPosition.y - dy / halfSide / scale;
+          Vec2.scale(d, d, this.map.logicalHalfSide / (this.map.halfSide * this.map.scale)) 
+          const pos = Vec2.clone(db.initialPosition)
+          Vec2.add(pos, pos, d) 
+          aim.pos = pos
         }
       }
     }
@@ -125,66 +133,86 @@ export default defineComponent({
       delete this.aimNetwork.dragCandidate; 
     }
 
+    const updateWhatever = (mouse: Vec2) : void => {
+      this.map.updateMouse(mouse)
+      if(this.map.panBeginning) {
+        updatePan(mouse); 
+      } else if (this.map.dragBeginning) {
+        updateDrag(mouse); 
+      }
+    }
+
+    canvas.addEventListener("mousemove", (e: MouseEvent) => {
+      updateWhatever([e.clientX, e.clientY])
+    })
 
     canvas.addEventListener("mousedown", (e: MouseEvent) => {
+      const mouse = Vec2.fromValues(e.clientX, e.clientY)
       if(this.aimNetwork.dragCandidate) {
-        beginDrag(this.aimNetwork.dragCandidate, e.pageX, e.pageY)
+        beginDrag(this.aimNetwork.dragCandidate, mouse)
       } else {
-        beginPan(e.pageX, e.pageY) 
+        beginPan(mouse) 
       }
     })
 
-    canvas.addEventListener("mouseup", (e: MouseEvent) => {
-      updateWhatever(e.pageX, e.pageY)
+    canvas.addEventListener("mouseup", () => {
       endWhatever() 
     })
 
     canvas.addEventListener("wheel", (e: WheelEvent) => {
-      const map = this.map
+      const mouse = Vec2.fromValues(e.clientX, e.clientY)
       const f = Math.pow(1.1, e.deltaY / 150)
-      let mouseBefore = pageToSVGCoords(e.pageX, e.pageY, map.offset, map.scale)
-      map.scale *= f
-      let mouseAfter = pageToSVGCoords(e.pageX, e.pageY, map.offset, map.scale) 
-      let delta = mouseAfter.clone().sub(mouseBefore)
-      map.offset.sub(delta) 
+      this.map.zoom(f, mouse) 
     })
 
+    let touch = {
+      currentCount: 0, 
+      dragFingerId: 0, 
+    }
+    let pinchBeginning: undefined | {
+      first: number, 
+      second: number, 
+      mLogical: V2, 
+      distancePage: number, 
+      offset: V2, 
+      scale: number, 
+    };
     canvas.addEventListener("touchstart", (e: TouchEvent) => { 
       if(e.touches.length > 0) {
         if(e.touches.length > 1) {
           // 2 or more touches         
-          if(touchStuff.count < 2) {
-            touchStuff.count = 2
-            let offset = this.map.offset.clone()
-            let scale = this.map.scale
+          if(touch.currentCount < 2) {
+            touch.currentCount = 2
             // page coordinates
-            let firstPage = new Vector2(e.touches[0].pageX, e.touches[0].pageY)
-            let secondPage = new Vector2(e.touches[1].pageX, e.touches[1].pageY)
-            let distancePage = firstPage.distanceTo(secondPage)
-            let middlePage = firstPage.add(secondPage).multiplyScalar(0.5)
-            let middleSVG = pageToSVGCoords(middlePage.x, middlePage.y, offset, scale) 
+            let firstPage = Vec2.fromValues(e.touches[0].clientY, e.touches[0].clientY)
+            let secondPage = Vec2.fromValues(e.touches[1].clientX, e.touches[1].clientY)
+            let mPhysical = Vec2.clone(firstPage) 
+            Vec2.add(mPhysical, mPhysical, secondPage) 
+            Vec2.scale(mPhysical, mPhysical, 0.5) 
+            let mLogical = this.map.physicalToLogicalCoord(mPhysical) 
             // model/svg coordinates
             pinchBeginning = {
               first: e.touches[0].identifier, 
               second: e.touches[1].identifier, 
-              middleSVG, 
-              distancePage, 
-              offset, 
-              scale 
+              mLogical, 
+              distancePage: Vec2.dist(firstPage, secondPage), 
+              offset: Vec2.clone(this.map.offset), 
+              scale: this.map.scale
             }
           } 
         } else {
           // 1 touch
-          if(touchStuff.count == 0) {
+          if(touch.currentCount == 0) {
             // new touch 
-            touchStuff.count = 1
-            touchStuff.dragFingerId = e.touches[0].identifier
+            touch.currentCount = 1
+            touch.dragFingerId = e.touches[0].identifier
+            let mouse = Vec2.fromValues(e.touches[0].clientX, e.touches[0].clientY)
             if(this.aimNetwork.dragCandidate) {
-              beginDrag(this.aimNetwork.dragCandidate, e.touches[0].pageX, e.touches[0].pageY)
+              beginDrag(this.aimNetwork.dragCandidate, mouse)
             } else {
-              beginPan(e.touches[0].pageX, e.touches[0].pageY)
+              beginPan(mouse)
             }
-          } else if (touchStuff.count > 1) {
+          } else if (touch.currentCount > 1) {
             // from 2 or more to 1
             pinchBeginning = undefined // end pinch
           }
@@ -192,73 +220,41 @@ export default defineComponent({
       }
     });
 
-    // const getTouch = (e: TouchEvent, identifier: number) : Touch | undefined => {
-    //   for(let touch of e.touches) {
-    //     if(touch.identifier == identifier) {
-    //       return touch
-    //     }
-    //   }
-    // }
-
     canvas.addEventListener("touchmove", (e: TouchEvent) => { 
-      if(touchStuff.count == 1) {
-        let touch = e.touches[0]
-        updateWhatever(touch.pageX, touch.pageY)
-      } else if (touchStuff.count > 1) {
+      if(touch.currentCount == 1) {
+        let mouse = Vec2.fromValues(e.touches[0].clientX, e.touches[0].clientY)
+        updateWhatever(mouse)
+      } else if (touch.currentCount > 1) {
         if(pinchBeginning) {
           // update pinch
           const map = this.map
 
-          let first = e.touches[0] // getTouch(e, pinchBeginning.first)
-          let second = e.touches[1] // getTouch(e, pinchBeginning.second)
+          let firstPage = Vec2.fromValues(e.touches[0].clientY, e.touches[0].clientY)
+          let secondPage = Vec2.fromValues(e.touches[1].clientX, e.touches[1].clientY)
+          let distancePage = Vec2.dist(firstPage, secondPage)
 
-          let firstPage = new Vector2(first.pageX, first.pageY)
-          let secondPage = new Vector2(second.pageX, second.pageY)
-          let distancePage = firstPage.distanceTo(secondPage)
-          let middlePage = firstPage.add(secondPage).multiplyScalar(0.5)
+          let mPhysical = Vec2.clone(firstPage) 
+          Vec2.add(mPhysical, mPhysical, secondPage) 
+          Vec2.scale(mPhysical, mPhysical, 0.5) 
+          let mLogical = this.map.physicalToLogicalCoord(mPhysical) 
 
+          // 
           let scaleChange = distancePage / pinchBeginning.distancePage 
-
-
           map.scale = pinchBeginning.scale * scaleChange
 
-          let middleSVG = pageToSVGCoords(
-            middlePage.x, 
-            middlePage.y, 
-            pinchBeginning.offset, 
-            map.scale
-          ) 
+          Vec2.sub(mLogical, mLogical, pinchBeginning.mLogical) 
 
-          let delta = middleSVG.clone().sub(pinchBeginning.middleSVG)
-
-          map.offset = pinchBeginning.offset.clone().sub(delta) 
-
-            // const f = Math.pow(1.1, e.deltaY / 150)
-            // let mouseBefore = pageToSVGCoords(e.pageX, e.pageY)
-            // map.scale *= f
-            // let mouseAfter = pageToSVGCoords(e.pageX, e.pageY) 
-            // let delta = mouseAfter.clone().sub(mouseBefore)
-            // map.offset.sub(delta) 
+          Vec2.sub(this.map.offset, this.map.offset, mLogical) 
         }
       }
     })
 
-    canvas.addEventListener("wheel", (e: WheelEvent) => {
-      const map = this.map
-      const f = Math.pow(1.1, e.deltaY / 150)
-      let mouseBefore = pageToSVGCoords(e.pageX, e.pageY, map.offset, map.scale)
-      map.scale *= f
-      let mouseAfter = pageToSVGCoords(e.pageX, e.pageY, map.offset, map.scale) 
-      let delta = mouseAfter.clone().sub(mouseBefore)
-      map.offset.sub(delta) 
-    })
-
     const finishTouch = () => {
-      if(touchStuff.count == 1) {
-        touchStuff.count = 0
+      if(touch.currentCount == 1) {
+        touch.currentCount = 0
         endWhatever()
-      } else if (touchStuff.count == 2) {
-        touchStuff.count = 0
+      } else if (touch.currentCount == 2) {
+        touch.currentCount = 0
       }
       this.map.preventReleaseClick = false // click not triggered after touch move
     }
@@ -266,32 +262,17 @@ export default defineComponent({
     canvas.addEventListener("touchend", finishTouch) 
     canvas.addEventListener("touchcancel", finishTouch) 
 
-    canvas.addEventListener("click", (e: MouseEvent) => {
-      store.commit(
-        MutationTypes.UPDATE_MOUSE_MAP_POSITION, 
-        pageToSVGCoords(
-          e.pageX, 
-          e.pageY, 
-          this.map.offset, 
-          this.map.scale
-        )
-      )
-      store.dispatch(ActionTypes.NOWHERE_CLICK)
-    });
-
-    const updateWhatever = (mouse: Vec2) : void => {
-      map.updateMouse(mouse)
-      if(map.panning !== undefined) {
-        updatePan(x, y); 
-      } else if (map.dragBeginning) {
-        updateDrag(x, y); 
-      }
-    }
+    // canvas.addEventListener("click", (e: MouseEvent) => {
+    //   //TBD nowhere click
+    // });
   }, 
   methods: {
 
   }, 
   computed: {
+    viewBox() : string {
+      return [-1,-1,2,2].map((v: number) => v * this.map.logicalHalfSide).join(' ')
+    }, 
     transform() : string {
       return [
         `scale(${this.map.scale})`, 
--- src/stores/map.ts
+++ src/stores/map.ts
@@ -12,32 +12,35 @@ export const useMap = defineStore('map', {
         logical: Vec2.fromValues(0,0), 
         physical: Vec2.fromValues(0,0)
       },
+      halfSide: 0, 
+      logicalHalfSide: 1000, 
       panBeginning: undefined as undefined | { page: V2, offset: V2 },
       dragBeginning: undefined as undefined | { page: V2, offset: V2 },
       preventReleaseClick: false,
+      clientOffset: Vec2.create(),
     }
   }, 
   actions: {
     updateMouse(physicalMouse: V2) {
       this.mouse.physical = physicalMouse
       this.mouse.logical = this.physicalToLogicalCoord(physicalMouse)
-
     }, 
     physicalToLogicalCoord(coord: V2) : V2 {
-      let halfSide, xOffset, yOffset
-      if(window.innerWidth < window.innerHeight) {
-        halfSide = window.innerWidth / 2
-        xOffset = 0
-        yOffset = (window.innerHeight - window.innerWidth) / 2
-      } else {
-        halfSide = window.innerHeight / 2
-        xOffset = (window.innerWidth - window.innerHeight) / 2
-        yOffset = 0
-      }
-      return Vec2.fromValues(
-        (1 - (coord[0] + xOffset) / halfSide) / this.scale - this.offset[0],
-        (1 - (coord[1] + yOffset) / halfSide) / this.scale - this.offset[1]
-      )
+      let result = Vec2.clone(coord) 
+      Vec2.sub(result, result, this.clientOffset) 
+      Vec2.scale(result, result, 1 / this.halfSide) 
+      Vec2.sub(result, result, [1,1]) 
+      Vec2.scale(result, result, this.logicalHalfSide / this.scale) 
+      Vec2.sub(result, result, this.offset) 
+      console.log(coord, "to", result) 
+      return result
+    }, 
+    zoom(f: number, mouse: V2) {
+      let mouseBefore = this.physicalToLogicalCoord(mouse)
+      this.scale *= f
+      let mouseAfter = this.physicalToLogicalCoord(mouse) 
+      Vec2.sub(mouseAfter, mouseAfter, mouseBefore) 
+      Vec2.add(this.offset, this.offset, mouseAfter) 
     }
   }
 })
