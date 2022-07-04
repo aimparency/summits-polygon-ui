14fef488af51b5041629753b63e991d1b0624a07
Felix Niemeyer <niemeyer.felix@gmail.com>
2022-07-04 20:11:10 +0200

    making it build fine

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    target: 'es2020',    
+    chunkSizeWarningLimit: 1000
+  },
   css: {
     preprocessorOptions: {
       less: {
