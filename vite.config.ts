099a08d134dece48a5ef66f6b34a040c2ed0c02d
Felix Niemeyer <niemeyer.felix@gmail.com>
2022-07-04 13:19:34 +0200

    shim instead of global

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    "global": {},
-  },  
   plugins: [vue()],
   css: {
     preprocessorOptions: {
